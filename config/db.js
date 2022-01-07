68a31c3663113f5a21e440a25afacbd655d0640f
Meurant <gaetanmeurant@gmail.com>
2022-01-07 14:51:42 +0100

    set default admin user logic

--- config/db.js
+++ config/db.js
@@ -3,10 +3,15 @@ const User = require('../schemas/User')
 
 const initializationDb = async () => {
     await mongoose.connect('mongodb://127.0.0.1:27017/hlj')
-    await User.create({
-        name: 'Admin',
-        email: 'admin@example.com'
-    })
+
+    mongoose.connection.db.collection('users').countDocuments(async (err, count) => {
+        if (count === 0) {
+            await User.create({
+                name: 'Admin',
+                email: 'admin@example.com'
+            })
+        }
+    });
 }
 
 module.exports = initializationDb
--- index.js
+++ index.js
@@ -2,9 +2,18 @@ require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
 const initializationDb = require("./config/db");
+const User = require("./schemas/User")
 
 
-const app =  express()
+const app = express()
+
+// test creation user
+async function createUser() {
+    await User.create({
+        name: 'test',
+        email: 'test@example.com',
+    })
+}
 
 initializationDb()
     .then(r => console.log('Database initialized'))
@@ -13,9 +22,16 @@ initializationDb()
 
 // route test
 app.get('/', (req, res) => {
-    res.status(200).json({message: 'Hello World !'})
+    res.status(200).json({ message: 'Hello World !' })
 })
 
+app.get('/users', async (req, res) => {
+    const users = await User.find({})
+    return res.status(200).json({ users })
+})
+
+
+// createUser().then(r => console.log('User created')).catch(e => console.log(e.message))
 
 const PORT = process.env.PORT
 app.listen(PORT, () => console.log(`Server started on ${PORT}`))
