8b6e9f0141855ce2fddee17d25356f72deb8b3bb
Meurant <gaetanmeurant@gmail.com>
2022-01-12 01:47:44 +0100

    fetchJson + .env + searchEnglishToKanji + searchKanji

--- controllers/ApiController.js
+++ controllers/ApiController.js
@@ -1,8 +1,27 @@
-import fetch from "node-fetch";
+import {fetchJson} from "../lib/fetchJson.js";
 
 export default class ApiController {
     static searchEnglishToKanji = async (req, res) => {
-        const data = await fetch(`https://kanjialive-api.p.rapidapi.com/api/public/search/${req.params.word}`)
-        return res.status(200).send(data)
+        //@Todo see bad request in url
+        try {
+            const response = await fetchJson(`/search/${req.params.word}`, {
+                headers:{'x-rapidapi-key': process.env.API_KEY}
+            })
+            return res.status(200).send(response)
+        } catch (e) {
+            return res.status(400).send(e.message)
+        }
     }
+
+    static searchKanji = async (req, res) => {
+        try {
+            const response = await fetchJson(`/kanji/${req.params.kanji}`, {
+                headers:{'x-rapidapi-key': process.env.API_KEY}
+            })
+            return res.status(200).send(response)
+        } catch (e) {
+            return res.status(400).send(e.message)
+        }
+    }
+
 }
--- routes/api.route.js
+++ routes/api.route.js
@@ -3,6 +3,7 @@ import ApiController from "../controllers/ApiController.js";
 
 const router = express.Router()
 
-router.get('/search/:world', ApiController.searchEnglishToKanji)
+router.get('/search/:word', ApiController.searchEnglishToKanji)
+router.get('/kanji-details/:kanji', ApiController.searchKanji)
 
 export default router
