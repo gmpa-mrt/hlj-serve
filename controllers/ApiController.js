c9946c75d8cf3843dce5511fdf0d95ce3948b3cc
Meurant <gaetanmeurant@gmail.com>
2022-01-16 19:20:42 +0100

    searchEnglishToKanjiDetails

--- controllers/ApiController.js
+++ controllers/ApiController.js
@@ -1,37 +1,70 @@
 import {fetchJson} from "../lib/fetchJson.js";
 
 export default class ApiController {
+    /* Default request to RapidApi */
     static searchEnglishToKanji = async (req, res) => {
         //@Todo see bad request in url
         try {
             const response = await fetchJson(`/search/${req.params.word}`, {
-                headers:{'x-rapidapi-key': process.env.API_KEY}
+                headers: { 'x-rapidapi-key': process.env.API_KEY }
             })
-            return res.status(200).send(response)
+            return res.status(200).json(response)
         } catch (e) {
-            return res.status(400).send(e.message)
+            return res.status(400).json({
+                message: e.message
+            })
         }
     }
 
     static getKanji = async (req, res) => {
         try {
             const response = await fetchJson(`/kanji/${req.params.kanji}`, {
-                headers:{'x-rapidapi-key': process.env.API_KEY}
+                headers: { 'x-rapidapi-key': process.env.API_KEY }
             })
-            return res.status(200).send(response)
+            return res.status(200).json(response)
         } catch (e) {
-            return res.status(400).send(e.message)
+            return res.status(400).json({
+                message: e.message
+            })
         }
     }
 
     static getAllKanji = async (req, res) => {
         const response = await fetchJson('/kanji/all', {
-            headers:{'x-rapidapi-key': process.env.API_KEY}
+            headers: { 'x-rapidapi-key': process.env.API_KEY }
         })
         try {
-            return res.status(200).send(response)
+            return res.status(200).json(response)
         } catch (e) {
-            return res.status(400).send(e.message)
+            return res.status(400).json({
+                message: e.message
+            })
+        }
+    }
+    /* Default request to RapidApi */
+
+    static searchEnglishToKanjiDetails = async (req, res) => {
+        try {
+            const data = await fetchJson(`/search/${req.params.word}`, {
+                headers: { 'x-rapidapi-key': process.env.API_KEY }
+            })
+
+            const kanji = data[0].kanji
+
+            try {
+                const response = await fetchJson(`/kanji/${kanji.character}`, {
+                    headers: { 'x-rapidapi-key': process.env.API_KEY }
+                })
+                return res.status(200).json(response)
+            } catch (e) {
+                return res.status(400).json({
+                    message: e.message
+                })
+            }
+        } catch (e) {
+            return res.status(400).json({
+                message: e.message
+            })
         }
     }
 }
--- routes/api.route.js
+++ routes/api.route.js
@@ -4,6 +4,7 @@ import ApiController from "../controllers/ApiController.js";
 const router = express.Router()
 
 router.get('/search/:word', ApiController.searchEnglishToKanji)
+router.get('/search/details/:word', ApiController.searchEnglishToKanjiDetails)
 router.get('/kanji-details/:kanji', ApiController.getKanji)
 router.get('/kanji/all', ApiController.getAllKanji)
 
