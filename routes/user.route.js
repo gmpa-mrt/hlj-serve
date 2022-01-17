12b8544a6d5b0ecf579cbeed65c32bfa5b355ac0
Meurant <gaetanmeurant@gmail.com>
2022-01-17 11:09:26 +0100

    add middlewares autJWT on user s routes

--- routes/user.route.js
+++ routes/user.route.js
@@ -6,13 +6,13 @@ import UserController from '../controllers/UserController.js'
 const router = express.Router()
 
 router.get('/users', authJWT, UserController.user_get_all)
-router.get('/users/:id', UserController.user_show)
+router.get('/users/:id',authJWT, UserController.user_show)
 router.get('/me', authJWT, UserController.user_who_am_i)
 router.post('/register', UserController.user_create)
 router.post('/add/kanji/:kanji', authJWT, UserController.user_add_kanji)
 router.post('/remove/kanji/:kanji', authJWT, UserController.user_remove_one_kanji)
 router.post('/remove/all/kanji', authJWT, UserController.user_remove_all_kanji)
-router.patch('/users/update/:id', UserController.user_update)
-router.delete('/users/delete/:id', UserController.user_destroy)
+router.patch('/users/update/:id',authJWT, UserController.user_update)
+router.delete('/users/delete/:id',authJWT, UserController.user_destroy)
 
 export default router
